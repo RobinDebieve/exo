9f8b7924ae8085cee9981f674864f031cfa7d325
Robin <robinrobix14@gmail.com>
2024-12-06 08:59:56 +0100

    add_dl

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installé');
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() =>
+            caches.match(event.request)
+        )
+    );
+});
